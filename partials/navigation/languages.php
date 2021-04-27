9cc4523a1b563f65f663e18032b61a94c55c516c
Collins Lagat <cklagat@gmail.com>
2021-04-27 11:04:52 +0300

    feat: move languages to separate file

---
+++ partials/navigation/languages.php
@@ -0,0 +1,216 @@
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-us"></span>
+    </div>
+    <div class="text">
+        <span class="country">United States</span>
+        <span class="language">English</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-ru"></span>
+    </div>
+    <div class="text">
+        <span class="country">Russia</span>
+        <span class="language">Russian</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-cn"></span>
+    </div>
+    <div class="text">
+        <span class="country">China</span>
+        <span class="language">Chinese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tz"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tanzania</span>
+        <span class="language">Swahili</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tn"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tunisia</span>
+        <span class="language">Arabic</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-br"></span>
+    </div>
+    <div class="text">
+        <span class="country">Brazil</span>
+        <span class="language">Portuguese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-es"></span>
+    </div>
+    <div class="text">
+        <span class="country">Spain</span>
+        <span class="language">Spanish</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-it"></span>
+    </div>
+    <div class="text">
+        <span class="country">Italy</span>
+        <span class="language">Italian</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-us"></span>
+    </div>
+    <div class="text">
+        <span class="country">United States</span>
+        <span class="language">English</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-ru"></span>
+    </div>
+    <div class="text">
+        <span class="country">Russia</span>
+        <span class="language">Russian</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-cn"></span>
+    </div>
+    <div class="text">
+        <span class="country">China</span>
+        <span class="language">Chinese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tz"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tanzania</span>
+        <span class="language">Swahili</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tn"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tunisia</span>
+        <span class="language">Arabic</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-br"></span>
+    </div>
+    <div class="text">
+        <span class="country">Brazil</span>
+        <span class="language">Portuguese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-es"></span>
+    </div>
+    <div class="text">
+        <span class="country">Spain</span>
+        <span class="language">Spanish</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-it"></span>
+    </div>
+    <div class="text">
+        <span class="country">Italy</span>
+        <span class="language">Italian</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-us"></span>
+    </div>
+    <div class="text">
+        <span class="country">United States</span>
+        <span class="language">English</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-ru"></span>
+    </div>
+    <div class="text">
+        <span class="country">Russia</span>
+        <span class="language">Russian</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-cn"></span>
+    </div>
+    <div class="text">
+        <span class="country">China</span>
+        <span class="language">Chinese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tz"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tanzania</span>
+        <span class="language">Swahili</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-tn"></span>
+    </div>
+    <div class="text">
+        <span class="country">Tunisia</span>
+        <span class="language">Arabic</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-br"></span>
+    </div>
+    <div class="text">
+        <span class="country">Brazil</span>
+        <span class="language">Portuguese</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-es"></span>
+    </div>
+    <div class="text">
+        <span class="country">Spain</span>
+        <span class="language">Spanish</span>
+    </div>
+</a>
+<a role="button" class="language-selector__language js-language-selector__language">
+    <div class="flag">
+        <span class="flag-icon flag-icon-it"></span>
+    </div>
+    <div class="text">
+        <span class="country">Italy</span>
+        <span class="language">Italian</span>
+    </div>
+</a>
